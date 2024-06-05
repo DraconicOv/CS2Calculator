42c8ed02d71f7302a7e67fe6edb08d402e9642fc
DraconicOv <theredlionzzw@gmail.com>
2024-06-05 13:33:26 -0400

    added functionality to number buttons

---
+++ script.js
@@ -0,0 +1,17 @@
+
+var input = document.getElementById('input'), // input/output button
+  number = document.querySelectorAll('.numbers div'), // number buttons
+  operator = document.querySelectorAll('.operators div'), // operator buttons
+  result = document.getElementById('result'), // equal button
+  clear = document.getElementById('clear'), // clear button
+  resultDisplayed = false; // flag to keep an eye on what output is displayed
+
+var currentValue = "";
+var currentOperator = "";
+
+for (var i = 0; i < number.length; i++) {
+  number[i].addEventListener("click", function(e) {
+    input.innerHTML += e.target.innerHTML;
+  });
+}
+
